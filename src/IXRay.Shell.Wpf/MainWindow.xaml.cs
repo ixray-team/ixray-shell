348cfab916037a2a0f39bcb556bb885a0acf2ec7
acidicMercury8 <acidicmercury8@outlook.com>
2023-04-02 15:34:57 +0300

    Add empty WPF project

---
+++ src/IXRay.Shell.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+using System.Windows;
+
+namespace IXRay.Shell.Wpf;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() => InitializeComponent();
+}
